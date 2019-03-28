edc082fe8a6da27b964255cea9dc0de0915177f9
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-03-28 21:07:58 +0100

    Update

--- Examples/Run-Manual.ps1
+++ Examples/Run-Manual.ps1
@@ -1,9 +1,6 @@
 Import-Module PSWinDocumentation.AD -Force
 
-$Forest = Get-WinADForestInformation -Verbose
-$Forest
+#$Forest = Get-WinADForestInformation -Verbose
+$Forest.FoundDomains.'ad.evotec.xyz'.DomainUsersExpiredInclDisabled | ft -a
+$Forest.FoundDomains.'ad.evotec.xyz'.DomainUsersAll | ft -a
 
-
-
-
-#Set-ADUser -PasswordLas
