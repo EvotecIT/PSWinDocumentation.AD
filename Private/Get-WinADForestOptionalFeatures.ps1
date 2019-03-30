2b21bf163358f72575321e8edc727f25daa3131d
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-03-30 10:22:03 +0100

    Updates

--- Private/Get-WinADForestOptionalFeatures.ps1
+++ Private/Get-WinADForestOptionalFeatures.ps1
@@ -1,31 +1,18 @@
 function Get-WinADForestOptionalFeatures {
     [CmdletBinding()]
-    param(
-
-    )
+    param()
     $OptionalFeatures = $(Get-ADOptionalFeature -Filter * )
     $Optional = [ordered]@{
         'Recycle Bin Enabled'                          = 'N/A'
         'Privileged Access Management Feature Enabled' = 'N/A'
     }
-    ### Fix Optional Features
     foreach ($Feature in $OptionalFeatures) {
         if ($Feature.Name -eq 'Recycle Bin Feature') {
-            if ("$($Feature.EnabledScopes)" -eq '') {
-                $Optional.'Recycle Bin Enabled' = $False
-            } else {
-                $Optional.'Recycle Bin Enabled' = $True
-            }
+            $Optional.'Recycle Bin Enabled' = $Feature.EnabledScopes.Count -gt 0
         }
         if ($Feature.Name -eq 'Privileged Access Management Feature') {
-            if ("$($Feature.EnabledScopes)" -eq '') {
-                $Optional.'Privileged Access Management Feature Enabled' = $False
-            } else {
-                $Optional.'Privileged Access Management Feature Enabled' = $True
-            }
+            $Optional.'Privileged Access Management Feature Enabled' = $Feature.EnabledScopes.Count -gt 0
         }
     }
     return $Optional
-    ### Fix optional features
-
 }
