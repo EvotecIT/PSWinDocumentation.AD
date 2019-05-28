4ee6dd5045a3e953bae4acc863d72e2e259d8ac7
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-05-28 22:04:14 +0200

    Update

--- Examples/Run-Manual-AD.ps1
+++ Examples/Run-Manual-AD.ps1
@@ -1,5 +1,5 @@
 Clear-Host
-Import-Module .\PSWinDocumentation.AD.psd1 -Force
+#Import-Module .\PSWinDocumentation.AD.psd1 -Force
 
-$Forest = Get-WinADForestInformation -Verbose
-$Forest.FoundDomains.'ad.evotec.xyz'
+#$Forest = Get-WinADForestInformation -Verbose
+$Forest.FoundDomains.'ad.evotec.xyz'.DomainUsers | Where-Object { $_.PasswordNeverExpires -eq $false } | Select Name, PasswordExpired, PasswordLastSet, 'PasswordLastChanged(Days)', DaysToExpire, DateExpiry | ft -a
