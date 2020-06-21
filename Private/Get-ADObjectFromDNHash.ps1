72a18cf9cb21ffa56e7d4d1708d61cf42a1d9235
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-06-21 20:31:39 +0200

    Rearranged files

---
+++ Private/Get-ADObjectFromDNHash.ps1
@@ -0,0 +1,24 @@
+﻿function Get-ADObjectFromDNHash {
+    [CmdletBinding()]
+    param (
+        [string[]] $DistinguishedName,
+        [hashtable] $ADCatalog,
+        [string] $Type = '',
+        [string] $Splitter # ', ' # Alternative for example [System.Environment]::NewLine
+    )
+    if ($null -eq $DistinguishedName) {
+        return
+    }
+    $FoundObjects = foreach ($DN in $DistinguishedName) {
+        if ($Type -eq '') {
+            $ADCatalog.$DN
+        } else {
+            $ADCatalog.$DN.$Type
+        }
+    }
+    if ($Splitter) {
+        return ($FoundObjects | Sort-Object) -join $Splitter
+    } else {
+        return $FoundObjects | Sort-Object
+    }
+}
